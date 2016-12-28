c9e6c5191e05e4e8fe9b985bad966c86e73ab920
Niti Nrw <niti.patel@flatironschool.com>
2016-12-27 22:09:09 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'byebug'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(name: params["name"], content: params["content"])
+    redirect to '/posts'
+    byebug
+  end
+
+  get '/posts/:id/edit' do
+    # byebug
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :delete
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
   end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,6 @@
-#Placeholder for a model
+class Post < ActiveRecord::Base
+  # def initialize(name: name, content: content)
+  #   @name = name
+  #   @content = content
+  # end
+end
